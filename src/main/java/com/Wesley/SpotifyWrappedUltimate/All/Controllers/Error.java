0cc5f136b22d4af2762407d2bcd1fa83d46c7825
Wesley Kiang <drace@Wesleys-MacBook-Air.local>
2022-04-14 23:11:04 -0700

    cancel

--- src/main/java/com/Wesley/SpotifyWrappedUltimate/All/Controllers/Error.java
+++ src/main/java/com/Wesley/SpotifyWrappedUltimate/All/Controllers/Error.java
@@ -2,8 +2,10 @@ package com.Wesley.SpotifyWrappedUltimate.All.Controllers;
 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+@RestController
 public class Error {
     @GetMapping("/callback?error=access_denied")
     public ModelAndView cancel (ModelMap model){
