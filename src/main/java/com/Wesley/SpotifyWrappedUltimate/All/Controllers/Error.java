2b8f6e2e97eaa58a94a56d247b72a9f6a15959d5
Wesley Kiang <drace@Wesleys-MacBook-Air.local>
2022-04-15 00:22:29 -0700

    bad request 400 fix

--- src/main/java/com/Wesley/SpotifyWrappedUltimate/All/Controllers/Error.java
+++ src/main/java/com/Wesley/SpotifyWrappedUltimate/All/Controllers/Error.java
@@ -2,13 +2,14 @@ package com.Wesley.SpotifyWrappedUltimate.All.Controllers;
 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Error {
-    @GetMapping("/callback?error=access_denied")
-    public ModelAndView cancel (ModelMap model){
+    @GetMapping("/callback")
+    public ModelAndView cancel (@RequestParam("error") String code, ModelMap model){
         model.addAttribute("attribute", "redirectWithRedirectPrefix");
         return new ModelAndView("redirect:/login.html", model);
     }
