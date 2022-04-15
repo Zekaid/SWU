cb8b9700909dec121b17cabfe89261063211f0b3
Wesley Kiang <drace@Wesleys-MacBook-Air.local>
2022-04-14 23:09:02 -0700

    cancel

---
+++ src/main/java/com/Wesley/SpotifyWrappedUltimate/All/Controllers/Error.java
@@ -0,0 +1,13 @@
+package com.Wesley.SpotifyWrappedUltimate.All.Controllers;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class Error {
+    @GetMapping("/callback?error=access_denied")
+    public ModelAndView cancel (ModelMap model){
+        model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        return new ModelAndView("redirect:/login.html", model);
+    }
+}
